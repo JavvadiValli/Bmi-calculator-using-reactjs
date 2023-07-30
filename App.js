2a4e3d73d0b371e338d8746682c094ca53ba3437
Javvadi Durga Padma Nagavalli <135938371+JavvadiValli@users.noreply.github.com>
2023-07-30 19:40:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+
+import BmiCalculator from "./components/Bmi-calc";
+function App() {
+    return (
+      <div className="App">
+     <BmiCalculator/>
+   </div>
+    );
+  }
+  
+  export default App;
